c58b3e098c324302e9555c4f4494bb4f5df09717
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-08-21 15:21:27 +0200

    config auth0

---
+++ utils/auth.ts
@@ -0,0 +1,34 @@
+import { redirect } from "next/navigation"
+import { auth0 } from "./auth0"
+import prisma from "./prisma"
+
+export async function authorize() {
+	const session = await auth0.getSession()
+
+	if (session) {
+		const user = await getUser(session.user.email!)
+		return {
+			email: user.email,
+			id: user.id,
+			nickname: session.user.nickname!,
+		}
+	}
+
+	redirect("/auth/login")
+}
+
+async function getUser(email: string) {
+	let user = await prisma.user.findFirst({
+		where: { email: { equals: email } },
+		select: { id: true, email: true },
+	})
+
+	// pokud se uživatel zrovna zaregistorval, tak si ho potřebuju přidat do vlastní databáze
+	if (user == null)
+		user = await prisma.user.create({
+			data: { email: email },
+			select: { id: true, email: true },
+		})
+
+	return user
+}
