ee3cea14271d1f6dd74d5aba99b68fb252e24c81
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-08-21 13:39:50 +0200

    add dashboard page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  // title: "Nabijim",
+  // description: "Jednoduchá aplikace pro vytváření statistiky nabíjení.",
+};
+
+export default async function Layout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return <>{children}</>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,31 @@
+import { ChargingStats } from "@/components/charging-stats"
+import {
+	RecentChargings,
+	RecentChargingsSkeleton,
+} from "@/components/recent-chargings"
+import { Button } from "@/components/ui/button"
+import { Plus, Zap } from "lucide-react"
+import { getTranslations } from "next-intl/server"
+
+export default async function Page() {
+	const t = await getTranslations("Dashboard")
+
+	return (
+		<div className="container mx-auto px-4 py-8 max-w-6xl">
+			<div className="text-center mb-8">
+				<Button
+					// onClick={() => setShowAddDialog(true)}
+					size="lg"
+					className="bg-emerald-600 hover:bg-emerald-700 text-white px-8 py-4 text-lg font-semibold shadow-lg hover:shadow-xl transition-all duration-200"
+				>
+					<Plus className="h-6 w-6 mr-2" />
+					{t("recordCharging")}
+				</Button>
+			</div>
+
+			<ChargingStats />
+
+			<RecentChargings />
+		</div>
+	)
+}
