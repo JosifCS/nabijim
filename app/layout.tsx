6a6bd88272820c19b97f9aa8650b4d4f8557e927
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-08-21 13:39:38 +0200

    root layout with header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,49 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { getLocale } from "next-intl/server";
-import { ReactNode } from "react";
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+import { getLocale } from "next-intl/server"
+import { ReactNode } from "react"
+import { Header } from "@/components/header"
 
 // inicializace konstant
-String.Empty = "";
-String.CR = "\r";
-String.LF = "\n";
-String.CRLF = "\r\n";
+String.Empty = ""
+String.CR = "\r"
+String.LF = "\n"
+String.CRLF = "\r\n"
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
-  title: "Nabijim",
-  description: "Jednoduchá aplikace pro vytváření statistiky nabíjení.",
-};
+	title: "Nabijim",
+	description: "Jednoduchá aplikace pro vytváření statistiky nabíjení.",
+}
 
 export default async function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: ReactNode;
+	children: ReactNode
 }>) {
-  const locale = await getLocale();
+	const locale = await getLocale()
 
-  return (
-    <html lang={locale}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang={locale}>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+			>
+				<Header />
+
+				<div className="pt-16 min-h-screen bg-gradient-to-br from-emerald-50 to-green-100 dark:from-emerald-950 dark:to-green-900">
+					{children}
+				</div>
+			</body>
+		</html>
+	)
 }
