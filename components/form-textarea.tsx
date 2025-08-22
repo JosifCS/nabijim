7b1e758c7833656dd3a906932ccbf15b7bfddada
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-08-22 11:04:32 +0200

    Create form-textarea.tsx

---
+++ components/form-textarea.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import { useId } from "react"
+import { useFormContext } from "./form"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+import { Textarea } from "./ui/textarea"
+import { Skeleton } from "./ui/skeleton"
+import { cx } from "class-variance-authority"
+
+type FormTextAreaProps = React.InputHTMLAttributes<HTMLTextAreaElement> & {
+	label?: string
+	rows?: number
+	skeleton?: boolean
+}
+
+export function FormTextArea({
+	label,
+	defaultValue,
+	name,
+	skeleton = false,
+	...props
+}: FormTextAreaProps) {
+	const form = useFormContext()
+	const id = useId()
+
+	return (
+		<div className="grid w-full items-center gap-1.5">
+			{label && <Label htmlFor={id}>{label}</Label>}
+			{skeleton ? (
+				<Skeleton
+					className={cx("h-20 w-full rounded-md", props.className)}
+				/>
+			) : (
+				<Textarea
+					id={id}
+					name={name}
+					defaultValue={
+						props.type == "file"
+							? undefined
+							: form.prevState?.[name ?? "x"] ?? defaultValue
+					}
+					onChange={form.onChange}
+					{...props}
+				/>
+			)}
+			{name && form.validationErrors?.[name] && (
+				<small className="text-red-700">
+					{form.validationErrors[name]}
+				</small>
+			)}
+		</div>
+	)
+}
