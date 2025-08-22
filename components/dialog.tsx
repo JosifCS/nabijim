6354f2a8cfbc3e15a2021cc23f086c9da542cfb5
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-08-22 10:58:38 +0200

    Create dialog.tsx

---
+++ components/dialog.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react"
+import {
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+} from "./ui/dialog"
+import { ActionDialog } from "./action-dialog"
+
+type DialogProps = {
+	title: string
+	description?: string
+	children?: ReactNode
+}
+
+export function Dialog({ title, children, description }: DialogProps) {
+	return (
+		<ActionDialog>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>{title}</DialogTitle>
+					{description && (
+						<DialogDescription>{description}</DialogDescription>
+					)}
+				</DialogHeader>
+				{children}
+			</DialogContent>
+		</ActionDialog>
+	)
+}
