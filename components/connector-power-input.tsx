f16699beb8d1502243cb922eeb8e31727861b12a
JosifCS <109961607+JosifCS@users.noreply.github.com>
2025-11-17 21:09:20 +0100

    Create connector-power-input.tsx

---
+++ components/connector-power-input.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { ChangeEvent, useState } from "react"
+import { FormInput } from "./common/form-input"
+import { Button } from "./ui/button"
+
+export function ConnectorPowerInput({
+	label,
+	defaultValue,
+}: {
+	label: string
+	defaultValue: number
+}) {
+	const [value, setValue] = useState(String(defaultValue))
+
+	function change(e: ChangeEvent<HTMLInputElement>) {
+		const v = e.currentTarget.value
+		setValue(v)
+	}
+	return (
+		<div className="flex gap-2">
+			<FormInput
+				type="number"
+				name="power"
+				placeholder={label}
+				value={value}
+				onChange={change}
+				step={1}
+				min={1}
+				max={5000}
+			/>
+			<div className="flex gap-2">
+				<Button variant="outline" onClick={() => setValue("11")}>
+					11 kW
+				</Button>
+				<Button variant="outline" onClick={() => setValue("50")}>
+					50 kW
+				</Button>
+				<Button variant="outline" onClick={() => setValue("200")}>
+					200 kW
+				</Button>
+			</div>
+		</div>
+	)
+}
